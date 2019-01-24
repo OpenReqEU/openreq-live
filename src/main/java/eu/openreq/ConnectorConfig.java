4b315d2c66f0c9d25a93192b45befea2c6edd3bb
Ralph Samer <ralph.samer@student.tugraz.at>
2019-01-24 17:25:47 +0100

    Disable TLS/SSL

--- src/main/java/eu/openreq/ConnectorConfig.java
+++ src/main/java/eu/openreq/ConnectorConfig.java
@@ -9,7 +9,7 @@ import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletCon
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ConnectorConfig {
 
     @Bean
